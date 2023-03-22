853f511b7c825e58b9b4002a713ba508c1e51fa1
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2023-03-22 15:37:06 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,8 +8,9 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/callname")
-async def post_name(name: Request):
-    return {"Hello": name.json()}
+async def post_name(data: dict):
+    name = data.get('name')
+    return {"Hello": name}
 
 @app.get("/callname/{name}")
 def read_name(name: str = None):
